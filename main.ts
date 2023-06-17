6726e8a559f7872018c977de30674bbe17b949de
Matteo <72705765+iamnomeutente@users.noreply.github.com>
2023-06-17 11:35:16 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ enum RadioMessage {
 }
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber < 2) {
-        pins.digitalWritePin(DigitalPin.P0, receivedNumber)
+        pins.digitalWritePin(DigitalPin.P1, receivedNumber)
         if (receivedNumber == 1) {
             basic.showString("ON")
         } else {
@@ -16,6 +16,14 @@ radio.onReceivedNumber(function (receivedNumber) {
             StopAlarm()
         } else if (receivedNumber == startingAlarmID) {
             basic.showIcon(IconNames.Sad)
+        } else if (receivedNumber == alarmActiveID) {
+            alarmActive = 1
+            basic.showIcon(IconNames.Yes)
+            basic.clearScreen()
+        } else if (receivedNumber == alarmInactiveID) {
+            alarmActive = 0
+            basic.showIcon(IconNames.No)
+            basic.clearScreen()
         } else {
             basic.showIcon(IconNames.Confused)
         }
@@ -95,51 +103,43 @@ input.onButtonPressed(Button.A, function () {
     SendONAnimation()
 })
 function StartAlarm () {
-    alarmActive = 1
+    movementDetected = 1
     radio.sendNumber(startingAlarmID)
     led.setBrightness(maxBrightness)
     music.setVolume(maxVolume)
-    while (alarmActive) {
+    while (movementDetected) {
         music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.UntilDone)
-        basic.showLeds(`
-            # . # . #
-            . # . # .
-            # . # . #
-            . # . # .
-            # . # . #
-            `)
-        basic.showLeds(`
-            . # . # .
-            # . # . #
-            . # . # .
-            # . # . #
-            . # . # .
-            `)
+        game.addScore(1)
     }
 }
 input.onButtonPressed(Button.AB, function () {
     if (input.isGesture(Gesture.LogoDown)) {
         radio.sendNumber(stoppingAlarmID)
+        basic.clearScreen()
+    } else if (input.isGesture(Gesture.TiltLeft)) {
+        radio.sendNumber(alarmActiveID)
+    } else if (input.isGesture(Gesture.TiltRight)) {
+        radio.sendNumber(alarmInactiveID)
     }
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(0)
     SendOFFAnimation()
 })
-input.onGesture(Gesture.Shake, function () {
-    StartAlarm()
-})
 function Init () {
     radio.setGroup(23)
     defaultBrightness = 26
-    alarmActive = 0
+    movementDetected = 0
     maxVolume = 255
     maxBrightness = 255
+    alarmActive = 1
+    alarmActiveID = 21
+    alarmInactiveID = 20
     startingAlarmID = 11
     stoppingAlarmID = 10
 }
 function StopAlarm () {
-    alarmActive = 0
+    movementDetected = 0
     music.stopAllSounds()
     led.setBrightness(defaultBrightness)
     basic.clearScreen()
@@ -169,15 +169,22 @@ function SendONAnimation () {
     basic.clearScreen()
 }
 let maxVolume = 0
+let movementDetected = 0
 let alarmActive = 0
+let alarmInactiveID = 0
+let alarmActiveID = 0
 let startingAlarmID = 0
 let stoppingAlarmID = 0
 let maxBrightness = 0
 let defaultBrightness = 0
 Init()
-pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P1, 0)
 led.setBrightness(defaultBrightness)
 StartingAnimation()
 basic.forever(function () {
-	
+    if (alarmActive) {
+        if (input.acceleration(Dimension.Y) >= 1800 || input.acceleration(Dimension.X) >= 1800) {
+            StartAlarm()
+        }
+    }
 })
